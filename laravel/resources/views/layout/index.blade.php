0cfb0ee324686901f822938fc505ffe70421e7bd
Hai Truong Cong <truonghaibg@gmail.com>
2018-08-23 00:41:47 +0700

    change style.css

--- laravel/resources/views/layout/index.blade.php
+++ laravel/resources/views/layout/index.blade.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="PROJECT 2017">
-    <meta name="author" content="">
-    <title>Công ty cổ phần PJM</title>
-    <base href="{{asset('')}}">
+    <title>Công ty cổ phần PJM | Vật liệu xây dựng hoàn thiện | Gạch ốp lát | Đá ốp lát | Thế giới Mosaic | Keo dán, vữa ốp lát | Công cụ, dụng cụ ốp lát | Chống thấm và hóa chất | Nội Thất Vệ sinh và Bếp.</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta property="og:title" content="Công ty cổ phần PJM | Vật liệu xây dựng hoàn thiện | Gạch ốp lát | Đá ốp lát | Thế giới Mosaic | Keo dán, vữa ốp lát | Công cụ, dụng cụ ốp lát | Chống thấm và hóa chất | Nội Thất Vệ sinh và Bếp.">
+    <meta property="description" content="Công ty cổ phần PJM | Vật liệu xây dựng hoàn thiện | Gạch ốp lát | Đá ốp lát | Thế giới Mosaic | Keo dán, vữa ốp lát | Công cụ, dụng cụ ốp lát | Chống thấm và hóa chất | Nội Thất Vệ sinh và Bếp.">
+    <meta name="description" content="Công ty cổ phần PJM | Vật liệu xây dựng hoàn thiện | Gạch ốp lát | Đá ốp lát | Thế giới Mosaic | Keo dán, vữa ốp lát | Công cụ, dụng cụ ốp lát | Chống thấm và hóa chất | Nội Thất Vệ sinh và Bếp.">
+    <meta property="og:description" content="Công ty cổ phần PJM | Vật liệu xây dựng hoàn thiện | Gạch ốp lát | Đá ốp lát | Thế giới Mosaic | Keo dán, vữa ốp lát | Công cụ, dụng cụ ốp lát | Chống thấm và hóa chất | Nội Thất Vệ sinh và Bếp.">
+    <meta property="robots" content="index,dofollow">
+    <meta property="" content="pjm, people, job, money, pjm.vn">
+    <meta property="og:image" content="{{url('/')}}/upload/slider/image_0.jpg">
+    <meta property="al:ios:url" content="">
+    <meta property="al:android:url" content="">
+    <meta property="al:android:class" content="">
+    <meta name="author" content="pjm.vn">
+    <meta name="copyright" content="pjm.vn">
+    <meta property="og:type" content="website">
 
     <!-- Mobile Specific Metas
     ================================================== -->
@@ -15,18 +25,18 @@
 
     <!-- Import New -->
     <link rel="shortcut icon" href="{{url('template_asset/images/icon/favicon.ico')}}" />
-    <script src="template_asset/js/jquery.min.js"></script>
-    <script src="template_asset/js/jquery.nivo.slider.pack.js"></script>
-    <script src="template_asset/js/jquery.elevatezoom.js"></script>
-    <script src="template_asset/js/script.js"></script>
-    <script src="template_asset/js/menu.js"></script>
+    <script src="{{url('/')}}/template_asset/js/jquery.min.js"></script>
+    <script src="{{url('/')}}/template_asset/js/jquery.nivo.slider.pack.js"></script>
+    <script src="{{url('/')}}/template_asset/js/jquery.elevatezoom.js"></script>
+    <script src="{{url('/')}}/template_asset/js/script.js"></script>
+    <script src="{{url('/')}}/template_asset/js/menu.js"></script>
 	
 	<script src="{{url('/')}}/js/owl.carousel.js"></script>
 	<script src="{{url('/')}}/js/owl.carousel.min.js"></script>
 	<script src="{{url('/')}}/js/jquery-ui.min.js"></script>
 	
-    <script src="template_asset/js/myscript.js"></script>
-    <script src="js/numeric/jquery.numeric.min.js"></script>
+    <script src="{{url('/')}}/template_asset/js/myscript.js"></script>
+    <script src="{{url('/')}}/js/numeric/jquery.numeric.min.js"></script>
     <link rel="stylesheet" href="{{url('template_asset/css/style.css')}}" type="text/css" />
 	<link rel="stylesheet" href="{{url('/')}}/owl.carousel.min.css" type="text/css" />
 	<link rel="stylesheet" href="{{url('/')}}/owl.theme.default.min.css" type="text/css" />
