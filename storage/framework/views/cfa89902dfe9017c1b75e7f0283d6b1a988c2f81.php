3d85224f37f39c8e30ac593771f6107e49c34a38
Hai Truong Cong <truonghaibg@gmail.com>
2018-08-23 23:30:23 +0700

    delete vendor

--- storage/framework/views/cfa89902dfe9017c1b75e7f0283d6b1a988c2f81.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<?php echo $__env->make('layout.product', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.index', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
